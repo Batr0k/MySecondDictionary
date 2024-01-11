b6424f8b484d5dff951f465c0263f9e603c38c18
Dmitriy <dima.drobot2001@gmail.com>
2024-01-11 21:47:39 +0300

    Release

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-
+from pathlib import Path
+from fastapi.staticfiles import StaticFiles
 from src.words.orm import SyncOrm
 
 app = FastAPI(title="SecondDictionary")
 templates = Jinja2Templates(directory="templates")
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get('/{item}/learned')
 def select_learned(item: str, request: Request):
@@ -33,4 +34,9 @@ def get_words(request: Request, item: str):
     return templates.TemplateResponse(
         request=request, name="index.html",
         context={"eng": SyncOrm.select_eng_words_or_phrases_orm(iswords).model_dump(), "h2": h2}
+    )
+@app.get('/')
+def start(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="start.html",
     )
